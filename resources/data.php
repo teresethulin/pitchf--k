04e4c2cf968819784a1cc31e9dc7175696fbc4dd
teresethulin <terese.thulin@gmail.com>
2019-10-23 14:19:12 +0200

    Update arrays in data.php

--- resources/data.php
+++ resources/data.php
@@ -2,5 +2,24 @@
 
 declare(strict_types=1);
 
-// This is the file where you can keep your data arrays such as articles and
-// authors.
+
+$author = [
+    ['name' => 'Author Name', 'id' => 1],
+    ['name' => 'Author Name', 'id' => 2],
+    ['name' => 'Author Name', 'id' => 3],
+    ['name' => 'Author Name', 'id' => 4],
+    ['name' => 'Author Name', 'id' => 5],
+];
+
+$articles = [
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[0]['name'], 'authorID' => 1, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80', 'content' => 'lorem ipsum', 'name' => $author[0]['name'], 'authorID' => 1, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[1]['name'], 'authorID' => 2, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[1]['name'], 'authorID' => 2, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[2]['name'], 'authorID' => 3, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[2]['name'], 'authorID' => 3, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[3]['name'], 'authorID' => 4, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[3]['name'], 'authorID' => 4, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[4]['name'], 'authorID' => 5, 'date' => 20191022, 'likes' => 3],
+    ['title' => 'Madonna releases collaboration on Soundcloud', 'image' => 'https://images.unsplash.com/photo-1513829596324-4bb2800c5efb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60', 'content' => 'lorem ipsum', 'name' => $author[4]['name'], 'authorID' => 5, 'date' => 20191022, 'likes' => 3],
+];

209238553ffbed867a36022da8ca5bb79dc55b23
teresethulin <terese.thulin@gmail.com>
2019-10-23 15:23:30 +0200

    Create function for sorting dates

--- resources/functions.php
+++ resources/functions.php
@@ -1,3 +1,17 @@
 <?php
 
 declare(strict_types=1);
+
+require __DIR__ . '/data.php';
+
+/**
+ * Sorts published articles by date, latest first.
+ *
+ * @param array $todaysDate
+ * @param array $datePublished
+ * @return int
+ */
+function sortArticlesByDate(array $todaysDate, array $datePublished): int
+{
+    return strtotime($datePublished['date']) - strtotime($todaysDate['date']);
+}

7ae359ded6f08226f5ad921f49a7ba4ff5c3e731
teresethulin <terese.thulin@gmail.com>
2019-10-23 13:49:52 +0200

    Create header.php

---
+++ resources/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css" />
+    <link rel="stylesheet" href="style.css">
+    <title>Pitchf**k</title>
+</head>
+
+<body>
+
+    <?php
+
+    require __DIR__ . '/data.php';
+
+    ?>

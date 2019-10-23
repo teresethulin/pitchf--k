6b6f7caf443249f77076d24fc4dbbc38e5b1ce9c
teresethulin <terese.thulin@gmail.com>
2019-10-23 10:36:27 +0200

    Added resources
    - data.php
    - functions.php
    - index.php

---
+++ resources/data.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+// This is the file where you can keep your data arrays such as articles and
+// authors.
---
+++ resources/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is the file where you can keep your HTML markup. We should always try to
+// keep us much logic out of the HTML as possible. Put the PHP logic in the top
+// of the files containing HTML or even better; in another PHP file altogether.
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title></title>
+    </head>
+    <body>
+    </body>
+</html>

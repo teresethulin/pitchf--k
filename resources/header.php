a4b2e1885d06038f770ff6658a96047f5c9be6e8
teresethulin <terese.thulin@gmail.com>
2019-10-29 17:41:55 +0100

    Deleted favicon

--- resources/header.php
+++ resources/header.php
@@ -6,12 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css" />
     <link rel="stylesheet" href="style.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
-    <meta name="theme-color" content="#ffffff">
     <title>Pitchf**k</title>
 </head>
 
